b4c9f1b8d81ce84f87c375b18f12d81f0c375b4b
rramdehol <rramdeholshift@gmail.com>
2015-12-10 00:30:08 -0500

    Added countdown.rb

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown (seconds)
+  time_now = seconds
+  while time_now > 0
+    puts "#{time_now} SECOND(S)!"
+    time_now -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (seconds)
+  time_now = seconds
+  while time_now > 0
+    puts "#{time_now} SECOND(S)!"
+    time_now -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
+
